f46db9ca1f21322db6918f61cfe072b18d1813a4
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-28 08:09:48 +0900

    Add edit page with template

--- cmd/gowiki/wiki.go
+++ cmd/gowiki/wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -22,8 +23,8 @@ func loadPage(title string) (*Page, error) {
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
-}
-return &Page{Title: title, Body: body}, nil
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +33,19 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
